a177948a57ee560ac1aa2196fc446cb4d77224d3
Rob Allen <themasterprawn@gmail.com>
2017-03-09 18:55:55 +0000

    Chaanged common setup

--- ps1/Setup-CommonTools.ps1
+++ ps1/Setup-CommonTools.ps1
@@ -1,7 +1,3 @@
-Param(
-    $LabIPAddressPattern = "192.168.0.*",
-    $VagrantIPPattern    = "192.168.10.*"
-)
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 
@@ -10,7 +6,7 @@ iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.p
 choco install git.install -y
 choco install classic-shell -y
 choco install nuget.commandline -y
-choco install bginfo -y 
+choco install bginfo -y
 
 # Set local administrator password
 $user = [adsi]"WinNT://localhost/Administrator,user"
@@ -18,20 +14,5 @@ $user.SetPassword("P@ssword1")
 $user.SetInfo()
 
 Install-WindowsFeature PowerShell-ISE
- <#
-# Rename the LAB interface
-Get-NetAdapter -InterfaceIndex (Get-NetIPAddress -IPAddress $LabIPAddressPattern).InterfaceIndex | 
-    Rename-NetAdapter -NewName Lab
-   
- 
-    
-# Ensure the primary IP address setup by vagrant is not registered
-Get-NetIPConfiguration -InterfaceIndex (Get-NetIPAddress -IPAddress $VagrantIPPattern).InterfaceIndex |
-    Get-NetConnectionProfile |
-    Where IPv4Connectivity -ne "NoTraffic" |
-Set-DnsClient -RegisterThisConnectionsAddress:$false -Verbose
 
-    # 
-    #>
-
-    choco install powershell -y #Need powershell 5, needs reboot
+choco install powershell -y #Need powershell 5, needs reboot
